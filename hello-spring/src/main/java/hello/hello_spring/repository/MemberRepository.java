6bba072fb95221f7020f181828f37331854b7b83
KOO612 <ksm99007@gmail.com>
2025-03-17 16:54:57 +0900

    04/21 저장소가 정해지지 않은 회원 로직

--- hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -10,3 +10,17 @@ public interface MemberRepository {
     Optional<Member> findByName(String name);
     List<Member> findAll();
 }
+
+/*
+Optional<T>
+값이 있을 수도 없을 수도 있다. -> 값이 null일 수도 있음
+자바 8부터 추가된 클래스
+null이 나올 수 있는 상황을 안전하게 처리하기 위해 사용
+*/
+
+/*
+List<T>
+여러 개의 데이터를 순서대로 모아 놓는 자료구조
+배열처럼 여러 개의 값을 저장 가능
+여러 개의 데이터를 순서 있게 담기 위해 사용
+ */
